2c0513e6a897699a83d6d04f758d0d6d09349469
intern-tieumomo <intern.tieumomo@gmail.com>
2020-08-21 16:16:49 +0700

    prepping_the_database

--- app/Employee.php
+++ app/Employee.php
@@ -2,13 +2,20 @@
 
 namespace App;
 
+use App\Task;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+	protected $fillable = [
+		'name',
+		'birthday',
+		'phone',
+		'email',
+	];
+
     public function tasks()
     {
-    	return $this->belongsToMany('App\Task');
+    	return $this->belongsToMany(Task::class);
     }
 }
--- app/Task.php
+++ app/Task.php
@@ -2,13 +2,17 @@
 
 namespace App;
 
+use App\Employee;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+	protected $fillable = [
+		'name',
+	];
+
     public function employees()
     {
-    	return $this->belongsToMany('App\Employee');
+    	return $this->belongsToMany(Employee::class);
     }
 }
