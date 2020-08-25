ecdb482923fd5f9592b0395efc94fd3741dab35d
Phạm Hồng Quân <68263304+intern-tieumomo@users.noreply.github.com>
2020-08-25 10:02:30 +0700

    prepping_the_database

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-	protected $fillable = [
-		'name',
-	];
+    protected $fillable = [
+        'name',
+    ];
 
     public function employees()
     {
