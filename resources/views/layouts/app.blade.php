b8e39c0cceaf4429cdd358417a10820255da77dd
intern-tieumomo <intern.tieumomo@gmail.com>
2020-08-27 11:09:12 +0700

    fix online link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <title>{{ trans('message.title') }}</title>
         <!-- Styles -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
         <!-- Fonts -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+        <link href="{{ asset('css/fontawesome.min.css') }}" rel='stylesheet' type='text/css'>
     </head>
     <body id="app-layout">
         <nav class="navbar navbar-default">
@@ -37,7 +37,7 @@
         @yield('content')
 
         <!-- JavaScripts -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     </body>
 </html>
